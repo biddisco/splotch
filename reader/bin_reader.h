6830549e29aa05f65d67894f4bb5e571706949ce
cgheller <cgheller@019ef673-9300-0410-806d-f16013bd1951>
2009-09-24 12:33:53 +0000

    essage=new bin reader
    git-svn-id: http://www.mpa-garching.mpg.de/svn/cosmo-group/kdolag/splotch@6520 019ef673-9300-0410-806d-f16013bd1951

---
+++ reader/bin_reader.h
@@ -0,0 +1,5 @@
+long bin_reader_tab (vector<particle_sim> * , float * , float * , 
+                     char*, int , int * , int , int );
+
+long bin_reader_block (vector<particle_sim> * , float * , float * , 
+                     char*, int , int * , int , int );
