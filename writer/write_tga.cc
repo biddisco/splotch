66f8f99f265287befebb05b6af4cb34be1fd9714
kdolag <kdolag@019ef673-9300-0410-806d-f16013bd1951>
2009-09-25 20:05:03 +0000

    Cosmetics in output
    git-svn-id: http://www.mpa-garching.mpg.de/svn/cosmo-group/kdolag/splotch@6535 019ef673-9300-0410-806d-f16013bd1951

--- writer/write_tga.cc
+++ writer/write_tga.cc
@@ -19,7 +19,7 @@ void write_tga(paramfile params, arr2<COLOUR> &pic, int res, string frame_name)
   int ycut0 = params.find<int>("ycut0",0);
   int ycut1 = params.find<int>("ycut1",res);
 
-  cout << "writing" << endl;
+  cout << " writing tga file" << endl;
   int yres=ycut1-ycut0;
   const byte header[18] = { 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     res%256, res/256, yres%256, yres/256, 24, 32 };
