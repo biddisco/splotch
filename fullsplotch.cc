51e73db4c2eb886684189bdb7c1cbf95861db41a
kdolag <kdolag@019ef673-9300-0410-806d-f16013bd1951>
2009-11-24 12:53:06 +0000

    New Gadget1 reader
    git-svn-id: http://www.mpa-garching.mpg.de/svn/cosmo-group/kdolag/splotch@6980 019ef673-9300-0410-806d-f16013bd1951

--- fullsplotch.cc
+++ fullsplotch.cc
@@ -4,6 +4,7 @@
 #include "cxxsupport/mpi_support.cc"
 #include "cxxsupport/cxxutils.cc"
 #include "reader/gadget_reader.cc"
+#include "reader/millenium_reader.cc"
 //#include "reader/enzo_reader.cc"
 #include "reader/bin_reader.cc"
 #include "writer/write_tga.cc"
