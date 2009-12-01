947098a060a354a45e3d240687206a7473be7dec
jinzf <jinzf@019ef673-9300-0410-806d-f16013bd1951>
2009-12-01 09:56:46 +0000

    render_as_thread
    git-svn-id: http://www.mpa-garching.mpg.de/svn/cosmo-group/kdolag/splotch@7024 019ef673-9300-0410-806d-f16013bd1951

--- splotch/splotchutils.h
+++ splotch/splotchutils.h
@@ -342,6 +342,103 @@ void render (const vector<particle_sim> &p, arr2<COLOUR> &pic,
 }
 
 #ifdef CUDA
+void render_as_thread (const vector<particle_splotch> &p, arr2<COLOUR> &pic, 
+      bool a_eq_e,double grayabsorb)
+      {
+      const float64 rfac=1.5;
+      const float64 powtmp = pow(Pi,1./3.);
+      const float64 sigma0=powtmp/sqrt(2*Pi);
+      const float64 bfak=1./(2*sqrt(Pi)*powtmp);
+	  exptable xexp(-20.);
+
+      int xres = pic.size1(), yres=pic.size2();
+      pic.fill(COLOUR(0,0,0));
+
+#ifdef VS
+	  work_distributor wd (xres,yres,xres,yres);
+#else
+      work_distributor wd (xres,yres,200,200);
+#endif //ifdef VS
+
+    #pragma omp parallel
+{
+      int chunk;
+    #pragma omp for schedule(dynamic,1)
+      for (chunk=0; chunk<wd.nchunks(); ++chunk)
+        {
+        int x0, x1, y0, y1;
+        wd.chunk_info(chunk,x0,x1,y0,y1);
+        arr2<COLOUR> lpic(x1-x0,y1-y0);
+        lpic.fill(COLOUR(0,0,0));
+        int x0s=x0, y0s=y0;
+        x1-=x0; x0=0; y1-=y0; y0=0;
+
+
+        for (unsigned int m=0; m<p.size(); ++m)
+          {
+          float64 r=p[m].r;
+          float64 posx=p[m].x, posy=p[m].y;
+          posx-=x0s; posy-=y0s;
+          float64 rfacr=rfac*r;
+
+		  //in one chunk this culling is not necessary as it was done in coloring
+          int minx=int(posx-rfacr+1);
+          if (minx>=x1) continue;
+          minx=max(minx,x0);
+          int maxx=int(posx+rfacr+1);
+          if (maxx<=x0) continue;
+          maxx=min(maxx,x1);
+          if (minx>=maxx) continue;
+          int miny=int(posy-rfacr+1);
+          if (miny>=y1) continue;
+          miny=max(miny,y0);
+          int maxy=int(posy+rfacr+1);
+          if (maxy<=y0) continue;
+          maxy=min(maxy,y1);
+          if (miny>=maxy) continue;
+
+		  COLOUR8 a=p[m].a, e, q;
+          if (!a_eq_e)
+            {
+            e=p[m].e;
+            q=COLOUR8(e.r/(a.r+grayabsorb),e.g/(a.g+grayabsorb),e.b/(a.b+grayabsorb));
+            }
+
+          float64 radsq = rfacr*rfacr;
+          float64 prefac1 = -0.5/(r*r*sigma0*sigma0);
+          float64 prefac2 = -0.5*bfak/p[m].ro;
+          for (int x=minx; x<maxx; ++x)
+            {
+            float64 xsq=(x-posx)*(x-posx);
+            for (int y=miny; y<maxy; ++y)
+              {
+              float64 dsq = (y-posy)*(y-posy) + xsq;
+              if (dsq<radsq)
+                {
+                float64 fac = prefac2*xexp(prefac1*dsq);
+                if (a_eq_e)
+                  {
+                  lpic[x][y].r += (fac*a.r);
+                  lpic[x][y].g += (fac*a.g);
+                  lpic[x][y].b += (fac*a.b);
+                  }
+                else
+                  {
+                  lpic[x][y].r = q.r+(lpic[x][y].r-q.r)*xexp(fac*a.r);
+                  lpic[x][y].g = q.g+(lpic[x][y].g-q.g)*xexp(fac*a.g);
+                  lpic[x][y].b = q.b+(lpic[x][y].b-q.b)*xexp(fac*a.b);
+                  }//if a_eq_e
+                }// if dsq<radsq
+              }//y
+            }//x
+          }//for particle[m]
+        for(int ix=0;ix<x1;ix++)
+          for(int iy=0;iy<y1;iy++)
+            pic[ix+x0s][iy+y0s]=lpic[ix][iy];
+        }//for this chunk
+}//#pragma omp parallel
+}
+
 
 #ifdef HOST_THREAD_RENDER
 //DWORD WINAPI render_thread (const vector<particle_splotch> &p, int start, int end, arr2<COLOUR> &pic, 
@@ -932,7 +1029,7 @@ extern "C"
 void getCuTransformParams(cu_param_transform &para_trans,
 paramfile &params, double c[3], double l[3], double s[3])
 {
-	cout<<endl<<"\nRetrieve parameters for device transformation\n"<<endl;
+//	cout<<endl<<"\nRetrieve parameters for device transformation\n"<<endl;
 
 	VECTOR	campos(c[0], c[1], c[2]), 
 			lookat(l[0], l[1], l[2]), 
