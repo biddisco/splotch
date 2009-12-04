6f15649271893bbbcb7e2b0e636075c8f3c7cce2
cgheller <cgheller@019ef673-9300-0410-806d-f16013bd1951>
2009-12-04 14:48:55 +0000

    MPI reader re-added
    git-svn-id: http://www.mpa-garching.mpg.de/svn/cosmo-group/kdolag/splotch@7059 019ef673-9300-0410-806d-f16013bd1951

--- fullsplotch.cc
+++ fullsplotch.cc
@@ -7,5 +7,8 @@
 #include "reader/millenium_reader.cc"
 //#include "reader/enzo_reader.cc"
 #include "reader/bin_reader.cc"
+#ifdef USE_MPI
+#include "reader/bin_reader_mpi.cc"
+#endif
 #include "writer/write_tga.cc"
 #include "splotch/splotch.cc"
