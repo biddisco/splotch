b94ec4abedc3e0fc370fda1bc39e96cf87c52231
kdolag <kdolag@019ef673-9300-0410-806d-f16013bd1951>
2009-12-15 17:27:28 +0000

    Update for CUDA support (not yet working)
    git-svn-id: http://www.mpa-garching.mpg.de/svn/cosmo-group/kdolag/splotch@7115 019ef673-9300-0410-806d-f16013bd1951

--- splotch/splotchutils.cc
+++ splotch/splotchutils.cc
@@ -9,57 +9,6 @@
 
 #include "kernel/transform.h"
 
-#ifdef CUDA
-	#include "cuda/splotch_cuda.h"
-
-	//just for now that it's still needed for cuda codes 2 Dec 2009
-	struct particle_splotch
-	{
-		float32 x,y,r,ro;
-		COLOUR	a, e;
-
-		particle_splotch (float32 x_, float32 y_, float32 r_, float32 ro_, const COLOUR &a_, const COLOUR &e_)
-		: x(x_), y(y_), r(r_), ro(ro_), a(a_), e(e_) {}
-		particle_splotch () {}
-	};
-	
-	//#define CUDA_TEST_FRAGMENT
-	
-	#ifdef CUDA_TEST_FRAGMENT
-		//the fragment buffer at host
-		unsigned long	posFragBufH=0;
-		cu_fragment_AeqE	fragBuf[30*100000*12];//for current test only! 10-29
-		cu_fragment_AeqE	fragBufWrittenByHost[30*100000*12];//for current test only! 10-29
-	#endif //ifdef CUDA_TEST_FRAGMENT
-
-	//a array used for debug only
-	#ifdef CUDA_TEST_COLORMAP
-		int		size_inout_buffer=0;	//for debug only.
-		float	inout_buffer[500000][5];//for debug only. ptype,input,r,g,b, 
-	#endif //if CUDA_TEST_COLORMAP
-
-	//#define CUDA_TEST_EXP
-	#ifdef CUDA_TEST_EXP
-		int		size_inout_buffer=0;	//for debug only.
-		float	inout_buffer[100000][2];//for debug only. in, out
-	#endif //if CUDA_TEST_EXP
-
-	//#define HOST_THREAD_RENDER
-	#ifdef HOST_THREAD_RENDER
-	struct particle_splotch;
-	struct	param_render_thread
-	{
-		cu_particle_splotch	*p;
-		int start, end;
-	    bool a_eq_e;
-		double grayabsorb;
-		cu_color pic[][800];
-//		void	**pic;
-	};
-	#endif //ifdef HOST_THREAD_RENDER
-#endif //if CUDA
-
-
 using namespace std;
 using namespace RAYPP;
 
@@ -76,7 +25,7 @@ void render (const vector<particle_sim> &p, arr2<COLOUR> &pic,
       pic.fill(COLOUR(0,0,0));
 
 #ifdef VS
-	  work_distributor wd (xres,yres,xres,yres);
+      work_distributor wd (xres,yres,xres,yres);
 #else
       work_distributor wd (xres,yres,200,200);
 #endif //ifdef VS
@@ -866,7 +815,7 @@ void particle_normalize(paramfile &params, vector<particle_sim> &p, bool verbose
       mpiMgr.allreduce_max(maxint[itype]);
       mpiMgr.allreduce_max(maxcol[itype]);
 
-      float minval_int = params.find<float>("intensity_	min"+dataToString(itype),minint[itype]);
+      float minval_int = params.find<float>("intensity_min"+dataToString(itype),minint[itype]);
       float maxval_int = params.find<float>("intensity_max"+dataToString(itype),maxint[itype]);
       float minval_col = params.find<float>("color_min"+dataToString(itype),mincol[itype]);
       float maxval_col = params.find<float>("color_max"+dataToString(itype),maxcol[itype]);
