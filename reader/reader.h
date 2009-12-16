81d4a2050dc40cb192d2b75aa8dce846cbeb0002
cgheller <cgheller@019ef673-9300-0410-806d-f16013bd1951>
2009-12-16 12:31:57 +0000

    fixed some bugs in the new cmakefile based architecture
    git-svn-id: http://www.mpa-garching.mpg.de/svn/cosmo-group/kdolag/splotch@7124 019ef673-9300-0410-806d-f16013bd1951

--- reader/reader.h
+++ reader/reader.h
@@ -7,5 +7,5 @@ void gadget_reader(paramfile &params, vector<particle_sim> &p, int snr, double *
 void gadget_millenium_reader(paramfile &params, vector<particle_sim> &p, int snr, double *time);
 long bin_reader_tab (paramfile &params, vector<particle_sim> &points, float *maxr, float *minr,int mype, int npes);
 long bin_reader_block (paramfile &params, vector<particle_sim> &points, float *maxr, float *minr, int mype, int npes);
-
+long bin_reader_block_mpi (paramfile &params, vector<particle_sim> &points, float *maxr, float *minr, int mype, int npes);
 #endif
