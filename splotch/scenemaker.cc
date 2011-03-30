5e9921871be93f44ff4d8b2db8fc225668b880cd
kdolag <kdolag@019ef673-9300-0410-806d-f16013bd1951>
2011-03-30 06:15:54 +0000

    Re-inserted code which was exidentially lost in last submit
    git-svn-id: http://www.mpa-garching.mpg.de/svn/cosmo-group/kdolag/splotch@12528 019ef673-9300-0410-806d-f16013bd1951

--- splotch/scenemaker.cc
+++ splotch/scenemaker.cc
@@ -373,9 +373,10 @@ void sceneMaker::fetchFiles(vector<particle_sim> &particle_data, double fidx)
 #else
   int simtype = params.find<int>("simtype");
 #endif
-  double frac=0;
-  int snr1 = int(fidx), snr2=snr1+1;
-  frac = fidx-snr1;
+  int spacing = params.find<double>("snapshot_spacing",1);
+  int snr1 = int(fidx/spacing)*spacing, snr2=snr1+spacing;
+  double frac=(fidx-snr1)/spacing;
+
   switch (simtype)
     {
     case 0:
@@ -403,14 +404,14 @@ void sceneMaker::fetchFiles(vector<particle_sim> &particle_data, double fidx)
         if (snr1_now!=snr1)
           {
           cout << " reading new1 " << snr1 << endl;
-          gadget_reader(params,interpol_mode,p1,id1,vel1,snr1,time1);
+          gadget_reader(params,interpol_mode,p1,id1,vel1,snr1,time1,boxsize);
           buildIndex(id1.begin(),id1.end(),idx1);
           snr1_now = snr1;
           }
         if (snr2_now!=snr2)
           {
           cout << " reading new2 " << snr2 << endl;
-          gadget_reader(params,interpol_mode,p2,id2,vel2,snr2,time2);
+          gadget_reader(params,interpol_mode,p2,id2,vel2,snr2,time2,boxsize);
           buildIndex(id2.begin(),id2.end(),idx2);
           snr2_now = snr2;
           }
@@ -418,7 +419,7 @@ void sceneMaker::fetchFiles(vector<particle_sim> &particle_data, double fidx)
       else
         {
         double dummy;
-        gadget_reader(params,interpol_mode,particle_data,id1,vel1,0,dummy);
+        gadget_reader(params,interpol_mode,particle_data,id1,vel1,0,dummy,boxsize);
         }
       break;
     case 3:
@@ -503,5 +504,34 @@ bool sceneMaker::getNextScene (vector<particle_sim> &particle_data,
 #else
   fetchFiles(particle_data,scn.fidx);
 #endif
+
+ if (params.find<bool>("Periodic",true)) 
+    {
+      int npart = particle_data.size();
+      double boxhalf = boxsize / 2;
+
+      if(mpiMgr.master())
+	cout << " doing parallel box wrap " << boxsize << endl;
+#pragma omp parallel
+      {
+	int m;
+#pragma omp for schedule(guided,1000)
+	for (m=0; m<npart; ++m)
+	  {
+	    if(particle_data[m].x - lookat.x > boxhalf)
+	      particle_data[m].x -= boxsize;
+	    if(lookat.x - particle_data[m].x > boxhalf)
+	      particle_data[m].x += boxsize;
+	    if(particle_data[m].y - lookat.y > boxhalf)
+	      particle_data[m].y -= boxsize;
+	    if(lookat.y - particle_data[m].y > boxhalf)
+	      particle_data[m].y += boxsize;
+	    if(particle_data[m].z - lookat.z > boxhalf)
+	      particle_data[m].z -= boxsize;
+	    if(lookat.z - particle_data[m].z > boxhalf)
+	      particle_data[m].z += boxsize;
+	  }
+      }
+    }
   return true;
   }
