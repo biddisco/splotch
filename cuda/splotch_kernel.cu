957181e70a9b524992cdfd58aa1f7ff99faf8c7c
mrivi <mrivi@019ef673-9300-0410-806d-f16013bd1951>
2011-10-21 15:41:02 +0000

    cuda version with booster and image written inside the device
    git-svn-id: http://www.mpa-garching.mpg.de/svn/cosmo-group/kdolag/splotch@15264 019ef673-9300-0410-806d-f16013bd1951

--- cuda/splotch_kernel.cu
+++ cuda/splotch_kernel.cu
@@ -1,10 +1,7 @@
-/*
-Try accelerating splotch with CUDA. July 2009.
-Copyright things go here.
-*/
+#ifndef __KERNEL__
+#define __KERNEL__
 
-//#include "splotch_kernel.h"
-#include "splotch_cuda.h"
+#include "cuda/splotch_cuda.h"
 
 //MACROs
 #define Pi 3.14159265358979323846264338327950288
@@ -20,28 +17,20 @@ Copyright things go here.
 #define MAXSIZE 1000
 
 /////////constant memory declaration /////////////////////
+
 __constant__ cu_color_map_entry dmap[MAXSIZE];
 __constant__ int ptype_points[10];
 __constant__ cu_param dparams;
 
-/////////help functions///////////////////////////////////
-__device__ float    my_asinh(float val)
-  {
-  return log(val+sqrt(1.+val*val));
-  }
+//help functions
 
-__device__ void my_normalize(float minv, float maxv, float &val)
-  {
-  if (minv!=maxv) val =  (val-minv)/(maxv-minv);
-  }
-
-__device__ void clamp (float minv, float maxv, float &val)
+__device__ __forceinline__ void clamp (float minv, float maxv, float &val)
   {
   val = min(maxv, max(minv, val));
   }
 
 //fetch a color from color table on device
-__device__ cu_color get_color(int ptype, float val, int mapSize, int ptypes)
+__device__ __forceinline__ cu_color get_color(int ptype, float val, int mapSize, int ptypes)
   {
   __shared__ int map_size;
   __shared__ int map_ptypes;
@@ -70,80 +59,23 @@ __device__ cu_color get_color(int ptype, float val, int mapSize, int ptypes)
   return clr;
   }
 
-__global__ void k_post_process(cu_color *pic, int n)
+//colorize by kernel
+__device__ void k_colorize
+  (cu_particle_splotch *p2, int mapSize, int types)
   {
   //first get the index m of this thread
-  int m=blockIdx.x *blockDim.x + threadIdx.x;
-  if (m >=n)
-    m =n;
+  //int m=blockIdx.x *blockDim.x + threadIdx.x;
+  //if (m >n) m =n;
 
-  //each pic[m] should do the same calc, so sequence does not matter!
-  pic[m].r =1.0 - exp( pic[m].r);
-  pic[m].g =1.0 - exp( pic[m].g);
-  pic[m].b =1.0 - exp( pic[m].b);
-  }
-
-__global__ void k_combine
-  (int minx, int miny, int maxx, int maxy, int xres, int yres,
-  cu_particle_splotch *p, int pStart, int pEnd, cu_fragment_AeqE *fbuf, cu_color *pic)
-  {
-  int m =blockIdx.x *blockDim.x + threadIdx.x;
-  int n =(maxx-minx)*(maxy-miny);
-  if (m >=n)
-    m =n;
-
-  //get global coordinate point(x,y) of this thread
-  int point_x, point_y;
-  get_xy_from_sn(m, minx, miny, maxy, point_x, point_y);
-
-  //go through all particles, for each particle p if point(x,y) is in its region
-  //p(minx,miny, maxx,maxy) do the following.
-  //find the sequencial number sn1 in p(minx,miny, maxx,maxy), the fragment we are looking
-  //for in fragment buffer is fragBuf[ sn1+p.posInFBuf ]
-  //grab the fragment f(deltaR,deltaG,deltaB)
-  //find the sequencial number sn2 of point(x,y) in the output pic.
-  //pic[sn2] += f
-  int sn1, sn2, local_x, local_y, fpos;
-  for (int i=pStart; i<=pEnd; i++)
-    {
-    if ( point_x >=p[i].minx && point_x<p[i].maxx &&
-         point_y >=p[i].miny && point_y<p[i].maxy)
-      {
-      local_x =point_x -p[i].minx;
-      local_y =point_y -p[i].miny;
-      get_sn_from_xy(local_x, local_y, p[i].maxy, p[i].miny,sn1);
-      fpos =sn1 +p[i].posInFragBuf;
-
-      get_sn_from_xy(point_x, point_y, yres,0, sn2);
-      pic[sn2].r +=fbuf[fpos].aR;
-      pic[sn2].g +=fbuf[fpos].aG;
-      pic[sn2].b +=fbuf[fpos].aB;
-      }
-    }
-  }
-
-//device render function k_render1
-__global__ void k_render1
-  (cu_particle_splotch *p, int nP,
-  void *buf, bool a_eq_e, float grayabsorb, int mapSize, int types)
-  {
-  //first get the index m of this thread
-  int m;
-
-  m =blockIdx.x *blockDim.x + threadIdx.x;
-  if (m >=nP)//m goes from 0 to nP-1
-    return;
-
-  // coloring
-  int ptype = p[m].type;
-  float col1=p[m].e.r,col2=p[m].e.g,col3=p[m].e.b;
+  int ptype = p2->type;
+  float col1=p2->e.r,col2=p2->e.g,col3=p2->e.b;
   clamp (0.0000001,0.9999999,col1);
   if (dparams.col_vector[ptype])
     {
     clamp (0.0000001,0.9999999,col2);
     clamp (0.0000001,0.9999999,col3);
     }
-  float intensity=p[m].I;
+  float intensity=p2->I;
   clamp (0.0000001,0.9999999,intensity);
   intensity *= dparams.brightness[ptype];
 
@@ -167,6 +99,23 @@ __global__ void k_render1
       else
       { e.r =e.g =e.b =0.0; }
     }
+  p2->e=e;
+
+  }
+
+//device render function k_render1
+__global__ void k_render1
+  (cu_particle_splotch *p, int nP, void *buf, int *index, 
+   bool a_eq_e, float grayabsorb, int mapSize, int types, int yres)
+{
+  //first get the index m of this thread
+  int m;
+  m =blockIdx.x *blockDim.x + threadIdx.x;
+  if (m >=nP)//m goes from 0 to nP-1
+    return;
+
+  // coloring
+  k_colorize(&(p[m]), mapSize, types);
 
   //make fbuf the right type
   cu_fragment_AeqE        *fbuf;
@@ -184,7 +133,7 @@ __global__ void k_render1
   const float radsq = 2.25*r*r;
   const float stp = -0.5/(r*r*sigma0*sigma0);
 
-  cu_color q; //e=p[m].e;
+  cu_color q, e=p[m].e;
   if (!a_eq_e)
    {
      q.r = e.r/(e.r+grayabsorb);
@@ -218,8 +167,8 @@ __global__ void k_render1
           fbuf[fpos].aG =0.0;
           fbuf[fpos].aB =0.0;
         }
-      //for each (x,y)
-      fpos++;
+        index[fpos] = x*yres+y;
+        fpos++;
       }//y
     }//x
   }
@@ -254,22 +203,20 @@ __global__ void k_render1
           fbuf1[fpos].qG =1.0;
           fbuf1[fpos].qB =1.0;
         }
-      //for each (x,y)
-      fpos++;
+        index[fpos] = x*yres+y;
+        fpos++;
       }//y
     }//x
   }
- }
-
+}
 
 //Transform by kernel
 __global__ void k_transform
   (cu_particle_sim *p, cu_particle_splotch *p2, int n)
   {
-
   //first get the index m of this thread
   int m=blockIdx.x *blockDim.x + threadIdx.x;
-  if (m >n) m =n;
+  if (m >n) return;
 
   //now do x,y,z
   float x,y,z;
@@ -301,6 +248,10 @@ __global__ void k_transform
   r *= rfac;
   p2[m].I = p[m].I/rfac;
 
+  p2[m].x = x;
+  p2[m].y = y;
+  p2[m].r = r;
+
   p2[m].isValid = false;
 
   // compute region occupied by the partile
@@ -321,67 +272,68 @@ __global__ void k_transform
   int maxy=int(y+rfacr+1);
   if (maxy<=0) return;
   maxy=min(maxy,dparams.yres);
-  if (miny>=maxy) return;
+  if (miny>=maxy) return; 
 
   p2[m].minx =minx;  p2[m].miny =miny;
   p2[m].maxx =maxx;  p2[m].maxy =maxy;
 
-  p2[m].isValid = true;
-  p2[m].x = x;
-  p2[m].y = y;
-  p2[m].r = r;
+
   p2[m].e.r = (float) p[m].e.r;
   p2[m].e.g = (float) p[m].e.g;
   p2[m].e.b = (float) p[m].e.b;
   p2[m].type = p[m].type;
-
+  p2[m].isValid = true;
   }
 
-
-//colorize by kernel
-__global__ void k_colorize
-  (int n, cu_particle_splotch *p2, int mapSize, int types)
-  {
-
-  //first get the index m of this thread
+__global__ void k_clear(int n, cu_color *pic)
+{
+   //first get the index m of this thread
   int m=blockIdx.x *blockDim.x + threadIdx.x;
   if (m >n) m =n;
- 
-  int ptype = p2[m].type;
-  float col1=p2[m].e.r,col2=p2[m].e.g,col3=p2[m].e.b;
-  clamp (0.0000001,0.9999999,col1);
-  if (dparams.col_vector[ptype])
-    {
-    clamp (0.0000001,0.9999999,col2);
-    clamp (0.0000001,0.9999999,col3);
-    }
-  float intensity=p2[m].I;
-  clamp (0.0000001,0.9999999,intensity);
-  intensity *= dparams.brightness[ptype];
 
-  cu_color e;
-  if (dparams.col_vector[ptype])   // color from file
-    {
-    e.r=col1*intensity;
-    e.g=col2*intensity;
-    e.b=col3*intensity;
-    }
-  else   // get color, associated from physical quantity contained in e.r, from lookup table
-    {
-  //first find the right entry for this ptype
-      if (ptype<types)
-      {
-        e = get_color(ptype, col1, mapSize, types);
-        e.r *= intensity;
-        e.g *= intensity;
-        e.b *= intensity;
-      }
-      else
-      { e.r =e.g =e.b =0.0; }
-    }
-  p2[m].e=e;
+   pic[m].r = 0.0;
+   pic[m].g = 0.0;
+   pic[m].b = 0.0;
+}
 
+__global__ void k_combine(int n, bool a_eq_e, cu_color *pic, int *index, void *fragBuf)
+{
+   //first get the index m of this thread
+  int m=blockIdx.x *blockDim.x + threadIdx.x;
+  if (m >=n) return;
+  
+  if (a_eq_e)
+  {
+    cu_fragment_AeqE *fragBufAeqE = (cu_fragment_AeqE *)fragBuf;
+    pic[index[m]].r += fragBufAeqE[m].aR;
+    pic[index[m]].g += fragBufAeqE[m].aG;
+    pic[index[m]].b += fragBufAeqE[m].aB;
   }
+  else
+  {
+    cu_fragment_AneqE *fragBufAneqE = (cu_fragment_AneqE *)fragBuf;
+    pic[index[m]].r += fragBufAneqE[m].aR *
+				   (pic[index[m]].r - fragBufAneqE[m].qR);
+    pic[index[m]].g += fragBufAneqE[m].aG *
+				   (pic[index[m]].g - fragBufAneqE[m].qG); 
+    pic[index[m]].b += fragBufAneqE[m].aB *
+                                   (pic[index[m]].b - fragBufAneqE[m].qB);
+  }
+}
 
 
+struct sum_op
+{
+  __host__ __device__
+  cu_fragment_AeqE operator()(cu_fragment_AeqE& p1, cu_fragment_AeqE& p2) const{
+
+    cu_fragment_AeqE sum;
+    sum.aR = p1.aR + p2.aR;
+    sum.aG = p1.aG + p2.aG;
+    sum.aB = p1.aB + p2.aB;
+    return sum; 
+   } 
+};
+
+#endif
 
