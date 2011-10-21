7c4875356613fcf5f4751e5a1f62c8f69922dc21
mrivi <mrivi@019ef673-9300-0410-806d-f16013bd1951>
2011-10-21 15:39:13 +0000

    cuda version with booster and image written inside the device
    git-svn-id: http://www.mpa-garching.mpg.de/svn/cosmo-group/kdolag/splotch@15260 019ef673-9300-0410-806d-f16013bd1951

--- cuda/splotch_cuda2.h
+++ cuda/splotch_cuda2.h
@@ -9,7 +9,7 @@ THREADFUNC host_thread_func(void *pinfo);
 THREADFUNC cu_thread_func(void *pinfo);
 
 
-// struct containing thread task info
+// struct containing pthread task info
 struct thread_info
   {
   int devID;                  //index of the device selected
@@ -21,26 +21,19 @@ struct thread_info
 
 //some global info shared by all threads
 extern paramfile       *g_params;
-extern std::vector<particle_sim> particle_data; //raw data from file
 extern vec3 campos, lookat, sky;
 extern std::vector<COLOURMAP> amap;
+extern std::vector<particle_sim> *particle_data;
 extern int ptypes;
+extern float b_brightness;
 extern wallTimerSet cuWallTimers;
 
 int check_device(int rank);
 void print_device_info(int rank, int dev);
 
-void cuda_rendering(int mydevID, int nDev, arr2<COLOUR> &pic);
+void cuda_rendering(int mydevID, int nDev, arr2<COLOUR> &pic, std::vector<particle_sim> &particle, float brightness);
 void DevideThreadsTasks(thread_info *tInfo, int nThread, bool bHostThread);
-void cu_draw_chunk(void *pinfo, cu_gpu_vars* gv);
-int filter_chunk(int StartP, int chunk_dim, int nParticle, int maxRegion,
-                 int nFBufInCell, cu_particle_splotch *cu_ps,
-                 cu_particle_splotch *cu_ps_filtered, int *End_cu_ps, 
-                 int *nFragments2Render);
-void render_chunk(int EndP, int nFBufInCell, cu_particle_splotch *cu_ps_filtered,                           void *fragBuf, cu_gpu_vars *gv, bool a_eq_e, float64 grayabsorb,
-                  arr2<COLOUR> &pPic, wallTimerSet &times);
-void combine_chunk(int StartP, int EndP, cu_particle_splotch *cu_ps_filtered, 
-                  void *fragBuf, bool a_eq_e, float64 grayabsorb, arr2<COLOUR> &pPic);
+//void cu_draw_chunk(void *pinfo, cu_gpu_vars* gv);
 void setup_colormap(int ptypes, cu_gpu_vars* gv);
 
 void GPUReport(wallTimerSet &cuTimers);
