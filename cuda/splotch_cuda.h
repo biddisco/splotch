f5a0ff535d60e92a99dec4cd5c2fe6473dc4990f
tdykes <tdykes@019ef673-9300-0410-806d-f16013bd1951>
2013-07-04 16:29:46 +0000

    Ranging Update (tim dykes)
    git-svn-id: http://www.mpa-garching.mpg.de/svn/cosmo-group/kdolag/splotch@24991 019ef673-9300-0410-806d-f16013bd1951

--- cuda/splotch_cuda.h
+++ cuda/splotch_cuda.h
@@ -45,6 +45,17 @@ struct cu_param
   float zmaxval, zminval;
   bool col_vector[MAX_P_TYPE];
   float brightness[MAX_P_TYPE];
+    //------------------------------------------------------------------------
+  // Ranging additions - Tim Dykes
+  bool log_int[MAX_P_TYPE];
+  bool log_col[MAX_P_TYPE];
+  bool asinh_col[MAX_P_TYPE];
+  float inorm_mins[MAX_P_TYPE];
+  float inorm_maxs[MAX_P_TYPE];
+  float cnorm_mins[MAX_P_TYPE];
+  float cnorm_maxs[MAX_P_TYPE];
+  bool do_logs;
+  //------------------------------------------------------------------------
   float bfak, h2sigma, sigma0, rfac;
   };
 
@@ -67,22 +78,26 @@ struct cu_gpu_vars //variables used by each gpu
   {
   CuPolicy            *policy;
   cu_particle_sim     *d_pd;             //device_particle_data
-  int		      *d_active; //  -1=non-active particle, -2=active big particle, n=number of tile, n+1=C3 particles 
+  int		              *d_active; //  -1=non-active particle, -2=active big particle, n=number of tile, n+1=C3 particles 
   int                 *d_index;
   cu_color            *d_pic;
   cu_color            *d_pic1;
   cu_color            *d_pic2;
   cu_color            *d_pic3;
-  int	      	      *d_tiles;	 //number of particles per tile
-  int	      	      *d_tileID; // tile ID
+  int	      	        *d_tiles;	 //number of particles per tile
+  int	      	        *d_tileID; // tile ID
   int                 colormap_size;
   int                 colormap_ptypes;
   };
 
 //functions
-int cu_init(int devID, long int nP, int ntiles, cu_gpu_vars* pgv, paramfile &fparams, const vec3 &campos, const vec3 &lookat, vec3 &sky, float b_brightness);
+int cu_init(int devID, long int nP, int ntiles, cu_gpu_vars* pgv, paramfile &fparams, const vec3 &campos, const vec3 &lookat, vec3 &sky, float b_brightness, bool& doLogs);
 int cu_copy_particles_to_device(cu_particle_sim* h_pd, unsigned int n, cu_gpu_vars* pgv);
 int cu_process (int n, cu_gpu_vars* pgv, int tile_sidex, int tile_sidey, int width, int nxtiles, int nytiles);
+//---------------------------------------------------------------------------------
+// Ranging Changes - Tim Dykes
+int cu_range(int nP, cu_gpu_vars* pgv);
+//---------------------------------------------------------------------------------
 void cu_init_colormap(cu_colormap_info info, cu_gpu_vars* pgv);
 void cu_render1
   (int nP, int grid, int block, bool a_eq_e, float grayabsorb, cu_gpu_vars* pgv, int tile_sidex, int tile_sidey, int width, int nxtiles);
